fd13bc17592fd9c7727b4b2e2ea0ab80c556a1b0
Dylan <dcren2010@gmail.com>
2021-05-25 08:20:47 -0400

    add comment redirect fun

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+	# That's a side-effect of the nesting set up. Each request for a comment has to keep track of the article to which the comment is attached, thus the initial call to the find method of the Article model to get the article in question
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)		#this calls the show action of the ArticlesController which in turn renders the show.html.erb template
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
