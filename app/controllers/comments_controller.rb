b403061cb2f8b186157e55523810a502acbc39bb
Dylan <dcren2010@gmail.com>
2021-05-25 09:04:41 -0400

    Deleting Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,17 @@
 class CommentsController < ApplicationController
-
-	# That's a side-effect of the nesting set up. Each request for a comment has to keep track of the article to which the comment is attached, thus the initial call to the find method of the Article model to get the article in question
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)		#this calls the show action of the ArticlesController which in turn renders the show.html.erb template
+    redirect_to article_path(@article)
   end
-
+ 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+ 
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
