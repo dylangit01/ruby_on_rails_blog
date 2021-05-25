e22da34ebd003fac0cdb3ce1873a892548ecba2c
Dylan <dcren2010@gmail.com>
2021-05-25 01:12:59 -0400

    create and show articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resource :articles
+  resources :articles
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
